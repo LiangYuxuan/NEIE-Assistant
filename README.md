e859cf8b8e6c2e80f44f99a933e2da131f17a4a6
LiangYuxuan <lyx@lyx.es>
2016-11-24 01:30:47 +0800

    finish basic function

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ A small, cross-platform program for NEIE.
 We found a [project](https://github.com/ranulldd/NEIE-Assistant) on github which is coded by Visual Basic, but it is hard to run on all platform expect Windows. So we decided to make this program.
 
 ### Process
-We are now working on the function encode and decode by C++.
+We finished the basic function of the software.
 
-The next plan is to finish HTTP communicate by Python.
+The next plan is to make it easier to use.
+
+### Usage
+*The project have not finished, and it won't be the way to use it finally.*
+
+Modify the value of var in main.py from line 242 to line 250.
 
 ## Contributing
 We are free to accept to any code. Everyone can fork it and pull request.
---
+++ main.py
@@ -0,0 +1,296 @@
+import sys, os, time, random
+import xml.etree.ElementTree as ET
+
+sys.path.append(os.getcwd() + '/library')
+import requests, library
+
+class learn_english:
+    def __init__(self, path, username, password, level):
+        self.path = path
+        self.username = username
+        self.password = password
+        self.level = level
+        self.available = 0
+
+    def __encode_object(self, obj):
+        for name in obj:
+            obj[name] = library.encode(obj[name])
+        return obj
+
+    def __decode_object(self, obj):
+        for name in obj:
+            obj[name] = library.decode(obj[name])
+        return obj
+
+    def __xml(self, method, head, data):
+        xml = '''<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Header>
+    <CredentialSoapHeader xmlns="http://www.open.com.cn">
+'''
+        xml += '      <Username>' + head['Username'] + '</Username>\n'
+        xml += '      <Password>' + head['Password'] + '</Password>\n'
+        xml += '''      <ClientCredential>83BCC79913F2331685F4851C4ED2DA72  E37438347941F22F6A873D18CE562AC9</ClientCredential>
+    </CredentialSoapHeader>
+  </soap:Header>
+  <soap:Body>
+'''
+        xml += '    <' + method + ' xmlns="http://www.open.com.cn">\n'
+        for name in data:
+            xml += '      <' + name + '>' + data[name] + '</' + name + '>\n'
+        xml += '    </' + method + '>\n'
+        xml += '''  </soap:Body>
+</soap:Envelope>
+'''
+        return xml
+
+    def __check_connect(self):
+        if self.available == 0:
+            r = requests.get('http://' + self.path + '/WebService/ServiceV3.asmx?WSDL', headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            text = r.text
+            if text.find('LoginListeningResponse') != -1:
+                self.available = 1
+        return
+
+    def __sopa_post(self, method, body):
+        head = {'Username': self.username, 'Password': self.password}
+        self.__encode_object(head)
+
+        headers = {'Content-Type': 'text/xml; charset=utf-8', 'SOAPAction': 'http://www.open.com.cn/' + method}
+        payload = self.__xml(method, head, body)
+
+        r = requests.post('http://' + self.path + '/WebService/ServiceV3.asmx', headers=headers, data=payload)
+        root = ET.fromstring(r.text)
+        result = {}
+        body = root[0][0]
+        for child in body:
+            if child.text != None:
+                result[child.tag[24:]] = child.text
+        return result
+
+    def LoginListening(self):
+        self.__check_connect()
+        data = {'UserName': self.username, 'Password': self.password, 'Version': '4.0', 'LevelID': self.level}
+        self.__encode_object(data)
+
+        result = self.__sopa_post('LoginListening', data)
+
+        self.__decode_object(result)
+        result['ReturnMessage'] = result['ReturnMessage'].decode('gbk')
+
+        try:
+            self.UserID = result['UserID']
+            self.UserNumber = result['UserNumber']
+        except:
+            pass
+        return result
+
+    def GetListeningProgress(self):
+        self.__check_connect()
+        data = {}
+        data['UserID'] = self.UserID
+        data['LevelID'] = self.level
+
+        result = self.__sopa_post('GetListeningProgress', data)
+
+        try:
+            self.UnitID = result['UnitID']
+        except:
+            self.UnitID = '1'
+
+        try:
+            self.SectionID = result['SectionID']
+        except:
+            self.SectionID = self.level + self.UnitID + '11'
+        return result
+
+    def SetListeningUnitLearnStaus(self, status):
+        self.__check_connect()
+        data = {}
+        data['UserID'] = self.UserID
+        data['LevelID'] = self.level
+        data['UnitID'] = self.UnitID
+        data['Status'] = status
+        self.__encode_object(data)
+
+        result = self.__sopa_post('SetListeningUnitLearnStaus', data)
+
+        self.__decode_object(result)
+        return result
+
+    def GetServerTime(self):
+        self.__check_connect()
+        data = {}
+
+        result = self.__sopa_post('GetServerTime', data)
+
+        self.BeginTime = result['GetServerTimeResult']
+        return result
+
+    def SetListeningResponseInformation(self, Score, IsCompleted):
+        self.__check_connect()
+        data = {}
+        data['UserID'] = self.UserID
+        data['UserNumber'] = self.UserNumber
+        data['SectionID'] = self.SectionID
+        data['SubSectionID'] = '0'
+        data['Response'] = ''
+        data['Score'] = Score
+        data['IsSaveResponse'] = '0'
+        data['IsCompleted'] = IsCompleted
+        data['BeginTime'] = self.BeginTime
+        self.__encode_object(data)
+
+        result = self.__sopa_post('SetListeningResponseInformation', data)
+
+        self.__decode_object(result)
+        return result
+
+    def SetListeningUserActiveInfo(self, SerialNumber, LicenseNumber, ActivationCode):
+        self.__check_connect()
+        data = {};
+        data['UserID'] = self.UserID
+        data['LevelID'] = self.level
+        data['SerialNumber'] = SerialNumber
+        data['LicenseNumber'] = LicenseNumber
+        data['ActivationCode'] = ActivationCode
+        data['IsActive'] = '2'
+        self.__encode_object(data)
+
+        result = self.__sopa_post('SetListeningUserActiveInfo', data)
+
+        self.__decode_object(result)
+        result['ReturnMessage'] = result['ReturnMessage'].decode('gbk')
+        return result
+
+def login(learn):
+    result = learn.LoginListening()
+    status = result['LoginListeningResult']
+    if status == '1':
+        library.warning('user ' + learn.username + ' need activation')
+        exit()
+    elif status != '2':
+        library.fail('user ' + learn.username + ' login failed: ' + result['ReturnMessage'])
+        exit()
+    else:
+        library.success('user ' + learn.username + ' login successfully')
+    return status
+
+def get_progress(learn):
+    result = learn.GetListeningProgress()
+    status = result['GetListeningProgressResult']
+    return status
+
+def update_unit(learn):
+    result = learn.SetListeningUnitLearnStaus('1')
+    status = result['SetListeningUnitLearnStausResult']
+    if status == '1':
+        library.success('update unit learn status success: unit '+ learn.UnitID)
+    else:
+        library.fail('update unit learn status fail: unit ' + learn.UnitID)
+        exit()
+    return status
+
+def end_section(learn, score):
+    result = learn.SetListeningResponseInformation("%.1f" % score, '1')
+    status = result['SetListeningResponseInformationResult']
+    if status == '1':
+        library.success('ended section ' + learn.SectionID + ' successfully: score ' + "%.1f" % score)
+    else:
+        library.fail('ended section ' + learn.SectionID + ' failed')
+        # Retry login
+        status = login(learn)
+        # Retry set response
+        result = learn.SetListeningResponseInformation("%.1f" % score, '1')
+        status = result['SetListeningResponseInformationResult']
+        if status == '1':
+            library.success('ended section ' + learn.SectionID + ' successfully: score ' + "%.1f" % score)
+        else:
+            library.fail('ended section ' + learn.SectionID + ' failed twice')
+            exit()
+    return status
+
+def start_section(learn):
+    result = learn.SetListeningResponseInformation('0', '0')
+    status = result['SetListeningResponseInformationResult']
+    if status == '1':
+        library.success('started section ' + learn.SectionID + ' successfully')
+    else:
+        library.fail('started section ' + learn.SectionID + ' failed')
+        exit()
+    return status
+
+# TOGO function active
+'''
+result = learn.LoginListening()
+status = result['LoginListeningResult']
+if status == '0':
+    result = learn.SetListeningUserActiveInfo('aaa', 'bbb', 'ccc')
+    if result['SetListeningUserActiveInfoResult'] != 1:
+        library.fail('user ' + learn.username + ' activation failed: ' + result['ReturnMessage'])
+        exit()
+    else:
+        library.success('user ' + learn.username + ' activation successfully')
+elif status != '2':
+    library.fail('user ' + learn.username + ' login failed: ' + result['ReturnMessage'])
+    exit()
+
+# as if need learn english
+raw_input()
+'''
+
+var = {}
+var['path'] = ''
+var['username'] = ''
+var['password'] = ''
+var['level'] = ''
+var['min_time'] = 60
+var['max_time'] = 120
+var['min_mark'] = 80
+var['max_mark'] = 100
+
+# Var init
+learn = learn_english(var['path'], var['username'], var['password'], var['level']);
+
+# Login
+status = login(learn)
+
+# Get Progress
+status = get_progress(learn)
+if status == '1':
+    fetch = library.fetch(learn.level, learn.SectionID)
+    if fetch['next']['unit'] != learn.UnitID:
+        update_unit(learn, fetch)
+        learn.UnitID = fetch['next']['unit']
+        learn.SectionID = fetch['next']['section']
+
+# Learn English
+while True:
+    learn.GetServerTime()
+    library.success('learning english: unit ' + learn.UnitID + ', section ' + learn.SectionID)
+    library.timer(random.randint(var['min_time'], var['max_time']))
+    library.success('finished learning: unit ' + learn.UnitID + ', section ' + learn.SectionID)
+
+    fetch = library.fetch(learn.level, learn.SectionID)
+    if fetch['problem'] == 0:
+        score = 0
+    else:
+        # Get int in next two var
+        min_problem = var['min_mark'] * fetch['problem'] / 100 + 1
+        max_problem = var['max_mark'] * fetch['problem'] / 100
+        if min_problem > max_problem:
+            min_problem = max_problem
+        score = random.randint(min_problem, max_problem) * 100.0 / fetch['problem']
+    status = end_section(learn, score)
+
+    try:
+        fetch['next']
+    except:
+        # end of all section
+        break;
+
+    if fetch['next']['unit'] != learn.UnitID:
+        status = update_unit(learn)
+    learn.SectionID = fetch['next']['section']
+
+    status = start_section(learn)
